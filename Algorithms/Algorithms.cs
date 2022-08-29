b17dfb9e3a570dd38afdb829f46a9b0f3101951d
Glenn Freeman <gfmetal@hotmail.com>
2022-08-29 10:31:01 -0400

    Add project files.

---
+++ Algorithms/Algorithms.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace Algorithms
+{
+    public static class Algorithms
+    {
+        public static int[] SymmetricDifference(params IEnumerable<int>[] arrays)
+        {
+            List<int> result = new();
+
+            foreach (int[] array in arrays)
+            {
+                List<int> combined = new();
+
+                foreach (var item in result)
+                {
+                    if (!array.Contains(item) && !combined.Contains(item)) combined.Add(item);
+                }
+
+                foreach (var item in array)
+                {
+                    if (!result.Contains(item) && !combined.Contains(item)) combined.Add(item);
+                }
+
+                result = combined;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ AlgorithmsTests/AlgorithmsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests
+{
+    [TestClass()]
+    public class AlgorithmsTests
+    {
+        [TestMethod()]
+        public void SymmetricDifferenceTest()
+        {
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 1, 2, 3 }, new int[] { 5, 2, 1, 4 }).OrderBy(x => x).SequenceEqual(new int[] { 3, 4, 5 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 1, 2, 3, 3 }, new int[] { 5, 2, 1, 4 }).OrderBy(x => x).SequenceEqual(new int[] { 3, 4, 5 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 1, 2, 3 }, new int[] { 5, 2, 1, 4, 5 }).OrderBy(x => x).SequenceEqual(new int[] { 3, 4, 5 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 1, 2, 5 }, new int[] { 2, 3, 5 }, new int[] { 3, 4, 5 }).OrderBy(x => x).SequenceEqual(new int[] { 1, 4, 5 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 1, 1, 2, 5 }, new int[] { 2, 2, 3, 5 }, new int[] { 3, 4, 5, 5 }).OrderBy(x => x).SequenceEqual(new int[] { 1, 4, 5 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 3, 3, 3, 2, 5 }, new int[] { 2, 1, 5, 7 }, new int[] { 3, 4, 6, 6 }, new int[] { 1, 2, 3 }).OrderBy(x => x).SequenceEqual(new int[] { 2, 3, 4, 6, 7 }));
+            Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 3, 3, 3, 2, 5 }, new int[] { 2, 1, 5, 7 }, new int[] { 3, 4, 6, 6 }, new int[] { 1, 2, 3 }, new int[] { 5, 3, 9, 8 }, new int[] { 1 }).OrderBy(x => x).SequenceEqual(new int[] { 1, 2, 4, 5, 6, 7, 8, 9 }));
+        }
+    }
+}
