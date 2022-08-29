c10f1b0c8cb8cb4c40996c4d28358e2b285a088d
metal-samurai <gfmetal@hotmail.com>
2022-08-29 17:23:16 -0400

    More exercises

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -2,8 +2,16 @@
 
 namespace Algorithms
 {
+    /// <summary>
+    /// For much of this I am intentionally avoiding LINQ for the sake of practice. In production much of this code would be unnecessary.
+    /// </summary>
     public static class Algorithms
     {
+        /// <summary>
+        /// Create a function that takes two or more arrays and returns an array of their symmetric difference. The returned array must contain only unique values (no duplicates).
+        /// </summary>
+        /// <param name="arrays"></param>
+        /// <returns></returns>
         public static int[] SymmetricDifference(params IEnumerable<int>[] arrays)
         {
             List<int> result = new();
@@ -27,5 +35,88 @@ namespace Algorithms
 
             return result.ToArray();
         }
+
+        public class InventoryItem : IComparable<InventoryItem>
+        {
+            public int Count { get; set; }
+            public string Name { get; set; } = string.Empty;
+
+            public int CompareTo(InventoryItem? other)
+            {
+                return this.Name.CompareTo(other?.Name);
+            }
+        }
+
+        /// <summary>
+        /// Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in current). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="delivery"></param>
+        public static void UpdateInventory(List<InventoryItem> current, IEnumerable<InventoryItem> delivery)
+        {
+            foreach (InventoryItem item in delivery)
+            {
+                var found = current.FindIndex(x => x.Name == item.Name);
+                if (found == -1) current.Add(item);
+                else current[found].Count += item.Count;
+            }
+
+            current.Sort();
+        }
+
+        public static List<T[]> GetPermutations<T>(T[] array)
+        {
+            List<T[]> result = new();
+
+            if (array.Length == 1) result.Add(array);
+            else
+            {
+                for (var i = 0; i < array.Length; i++)
+                {
+                    T first = array[i];
+                    List<T> remainder = array.ToList();
+                    remainder.RemoveAt(i);
+
+                    foreach (T[] perm in GetPermutations(remainder.ToArray()))
+                    {
+                        List<T> permutation = new() { first };
+                        permutation.AddRange(perm);
+                        result.Add(permutation.ToArray());
+                    }
+                }
+            }
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Return the number of total permutations of the provided string that don't have repeated consecutive letters.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int PermAlone(string input)
+        {
+            int permCount = 0;
+            
+            foreach (char[] permutation in GetPermutations(input.ToCharArray()))
+            {
+                bool repeat = false;
+
+                for (var i = 0; i < permutation.Length - 1; i++)
+                {
+                    if (permutation[i] == permutation[i + 1])
+                    {
+                        repeat = true;
+                        break;
+                    }
+                }
+
+                if (!repeat) permCount++;
+            }
+
+            return permCount;
+        }
     }
+
+    
 }
--- AlgorithmsTests/AlgorithmsTests.cs
+++ AlgorithmsTests/AlgorithmsTests.cs
@@ -22,5 +22,91 @@ namespace Algorithms.Tests
             Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 3, 3, 3, 2, 5 }, new int[] { 2, 1, 5, 7 }, new int[] { 3, 4, 6, 6 }, new int[] { 1, 2, 3 }).OrderBy(x => x).SequenceEqual(new int[] { 2, 3, 4, 6, 7 }));
             Assert.IsTrue(Algorithms.SymmetricDifference(new int[] { 3, 3, 3, 2, 5 }, new int[] { 2, 1, 5, 7 }, new int[] { 3, 4, 6, 6 }, new int[] { 1, 2, 3 }, new int[] { 5, 3, 9, 8 }, new int[] { 1 }).OrderBy(x => x).SequenceEqual(new int[] { 1, 2, 4, 5, 6, 7, 8, 9 }));
         }
+
+        [TestMethod()]
+        public void UpdateInventoryTest1()
+        {
+            List<Algorithms.InventoryItem> current = new(new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 21, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 2, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 1, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 5, Name = "Microphone" } });
+            Algorithms.InventoryItem[] delivery = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 2, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 3, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 67, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 7, Name = "Toothpaste" } };
+            Algorithms.InventoryItem[] expected = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 88, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 2, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 3, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 3, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 5, Name = "Microphone" }, new Algorithms.InventoryItem() { Count = 7, Name = "Toothpaste" } };
+
+            Algorithms.UpdateInventory(current, delivery);
+
+            Assert.AreEqual(expected.Length, current.Count);
+            
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Name, current[i].Name);
+                Assert.AreEqual(expected[i].Count, current[i].Count);
+            }
+        }
+
+        [TestMethod()]
+        public void UpdateInventoryTest2()
+        {
+            List<Algorithms.InventoryItem> current = new(new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 21, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 2, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 1, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 5, Name = "Microphone" } });
+            Algorithms.InventoryItem[] delivery = Array.Empty<Algorithms.InventoryItem>();
+            Algorithms.InventoryItem[] expected = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 21, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 2, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 1, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 5, Name = "Microphone" } };
+
+            Algorithms.UpdateInventory(current, delivery);
+
+            Assert.AreEqual(expected.Length, current.Count);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Name, current[i].Name);
+                Assert.AreEqual(expected[i].Count, current[i].Count);
+            }
+        }
+
+        [TestMethod()]
+        public void UpdateInventoryTest3()
+        {
+            List<Algorithms.InventoryItem> current = new();
+            Algorithms.InventoryItem[] delivery = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 2, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 3, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 67, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 7, Name = "Toothpaste" } };
+            Algorithms.InventoryItem[] expected = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 67, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 2, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 3, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 7, Name = "Toothpaste" } };
+
+            Algorithms.UpdateInventory(current, delivery);
+
+            Assert.AreEqual(expected.Length, current.Count);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Name, current[i].Name);
+                Assert.AreEqual(expected[i].Count, current[i].Count);
+            }
+        }
+
+        [TestMethod()]
+        public void UpdateInventoryTest4()
+        {
+            List<Algorithms.InventoryItem> current = new(new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 0, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 0, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 0, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 0, Name = "Microphone" } });
+            Algorithms.InventoryItem[] delivery = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 1, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 1, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 1, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 1, Name = "Toothpaste" } };
+            Algorithms.InventoryItem[] expected = new Algorithms.InventoryItem[] { new Algorithms.InventoryItem() { Count = 1, Name = "Bowling Ball" }, new Algorithms.InventoryItem() { Count = 0, Name = "Dirty Sock" }, new Algorithms.InventoryItem() { Count = 1, Name = "Hair Pin" }, new Algorithms.InventoryItem() { Count = 1, Name = "Half-Eaten Apple" }, new Algorithms.InventoryItem() { Count = 0, Name = "Microphone" }, new Algorithms.InventoryItem() { Count = 1, Name = "Toothpaste" } };
+
+            Algorithms.UpdateInventory(current, delivery);
+
+            Assert.AreEqual(expected.Length, current.Count);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Name, current[i].Name);
+                Assert.AreEqual(expected[i].Count, current[i].Count);
+            }
+        }
+
+        [TestMethod()]
+        public void PermAloneTest()
+        {
+            Assert.AreEqual(2, Algorithms.PermAlone("aab"));
+            Assert.AreEqual(0, Algorithms.PermAlone("aaa"));
+            Assert.AreEqual(8, Algorithms.PermAlone("aabb"));
+            Assert.AreEqual(3600, Algorithms.PermAlone("abcdefa"));
+            Assert.AreEqual(2640, Algorithms.PermAlone("abfdefa"));
+            Assert.AreEqual(0, Algorithms.PermAlone("zzzzzzzz"));
+            Assert.AreEqual(1, Algorithms.PermAlone("a"));
+            Assert.AreEqual(0, Algorithms.PermAlone("aaab"));
+            Assert.AreEqual(12, Algorithms.PermAlone("aaabb"));
+        }
     }
 }
